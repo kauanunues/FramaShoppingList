e4d83901dd0de8330acd61dab229c06a5670e621
mandinha2108 <amafur8@gmail.com>
2024-05-06 11:51:25 -0300

    estilização da lista de produtos

--- FramaShoppingList/src/app/home/index.js
+++ FramaShoppingList/src/app/home/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, ImageBackground, TextInput, TouchableOpacity} from 'react-native';
+import { Text, View, SafeAreaView, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import {Ionicons} from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { styles } from './style';
 
 export default function Home() {
   return (
-    <SafeAreaView style={{ flex :1 }}>
+    <SafeAreaView style={{ flex: 1 }}>
       <ImageBackground
         source={require('../../assets/background.jpg')}
         resizeMode='repeat'
-        style={{flex: 1, justifyContent: 'flex-start'}}
+        style={{ flex: 1, justifyContent: 'flex-start' }}
       >
         <View style={styles.header}>
           <Text style={styles.title}>Lista de Produtos</Text>
@@ -17,13 +18,23 @@ export default function Home() {
         </View>
 
         {/* Lista de produtos*/}
-        <View style={styles.footer}>
 
+        <View style={styles.footer}>
+          <View style={styles.inputContainer}>
+            <TextInput 
+              color= '#fff'
+              fontSize={18}
+              placeholderTextColor='#aeaeae'
+              placeholder='Digite o Nome do Produto...'
+            />
+          </View>
+          <TouchableOpacity style={styles.iconContainer}>
+            <Ionicons name= 'add' size={36} color='#fff'/>
+          </TouchableOpacity>
         </View>
+
       </ImageBackground>
       <StatusBar style="light" backgroundColor='#000' />
     </SafeAreaView>
   )
 }
-
-const styles = StyleSheet.create({})
