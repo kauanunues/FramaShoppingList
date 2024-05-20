905b3bac35246e86d368bcb15b4d934fa0fbbfdf
mandinha2108 <amafur8@gmail.com>
2024-05-20 12:24:20 -0300

    finalização da interface

--- FramaShoppingList/src/app/home/index.js
+++ FramaShoppingList/src/app/home/index.js
@@ -1,11 +1,49 @@
-import React from 'react'
-import { Text, View, SafeAreaView, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react'
+import { Text, View, SafeAreaView, ImageBackground, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Ionicons } from '@expo/vector-icons';
 import { styles } from './style';
 import ItemList from '../../components/ItemList';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Home() {
+ const [textInput, setTextInput] = useState('');
+ const [items, setItems ] = useState([]);
+ 
+ const saveItemToDevice = async  () => {
+
+ }
+
+ const getItemsFromDevice = async() => {
+
+ }
+ 
+ const addItem = () => {
+
+ }
+
+ const markItemBought = itemId => {
+
+ }
+
+ const unmarkItemBought = itemId => {
+
+ }
+ 
+ const removeItem = itemId => {
+
+ }
+
+ const removeAll = () => {
+  Alert.alert('Limpar lista?', 'Confirma a exclusão de todos os produtos da sua lista?', 
+    [{
+      text:'Sim',
+      onPress:() => {setItems([])}
+    },{
+      text:'Cancelar',
+      style:'cancel',
+    }])
+ }
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ImageBackground
@@ -15,12 +53,22 @@ export default function Home() {
       >
         <View style={styles.header}>
           <Text style={styles.title}>Lista de Produtos</Text>
-          <Ionicons name="trash" size={32} color='#fff' />
+          <Ionicons name="trash" size={32} color='#fff' onPress={removeAll} />
         </View>
 
         {/* Lista de produtos*/}
-        <ItemList item = {"Café"}></ItemList>
-        <ItemList item = {"Leite"}></ItemList>
+        <FlatList
+          contentContainerStyle={{ padding: 20, paddingBottom: 100, color:'#fff' }}
+          data = {items}
+          renderItem={({item}) => 
+            <ListItem item = {item}
+             markItem={markItemBought}
+             unmarkItem={unmarkItemBought}
+             removeItem={removeItem}
+            />
+          }
+        />
+        
 
         <View style={styles.footer}>
           <View style={styles.inputContainer}>
@@ -29,9 +77,11 @@ export default function Home() {
               fontSize={18}
               placeholderTextColor='#aeaeae'
               placeholder='Digite o Nome do Produto...'
+              value={textInput}
+              onChangeText={(text) => setTextInput(text)}
             />
           </View>
-          <TouchableOpacity style={styles.iconContainer}>
+          <TouchableOpacity style={styles.iconContainer} onPress={addItem}>
             <Ionicons name= 'add' size={36} color='#fff'/>
           </TouchableOpacity>
         </View>
--- FramaShoppingList/src/components/ItemList/index.js
+++ FramaShoppingList/src/components/ItemList/index.js
@@ -7,15 +7,29 @@ export default function ItemList({item, markItem, unmarkItem, removeItem}) {
     return (
       <View style={styles.itemList}>
         <View style={{ flex: 1 }}>  
-           <Text style={styles.itemToBuy}>{item}</Text>
+           <Text style={item?.bought ? styles.itemBought : styles.itemToBuy}>
+            {item?.name}
+           </Text>
         </View>
-
-        <TouchableOpacity 
-          style={styles.actionIcon}
-           onPress={() => markItem(item)}
+        {!item.bought ? (
+          
+       <TouchableOpacity 
+        style={styles.actionIcon}
+         onPress={() => markItem(item)}
         >
-          <Ionicons name='bag-check-outline' size={24} color="#fff" />
+        <Ionicons name='bag-check-outline' size={24} color="#fff" />
+       </TouchableOpacity>
+
+        ) : ( 
+          
+        <TouchableOpacity 
+        style={styles.actionIcon}
+         onPress={() => unmarkItem(item)}
+        >
+        <Ionicons name='bag-remove-outline' size={24} color="#fff" />
         </TouchableOpacity>
+        )}
+
 
         <TouchableOpacity 
           style={[styles.actionIcon, { backgroundColor: 'darkred'}]}
