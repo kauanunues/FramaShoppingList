63a032de6b243834bff38ab38d84e9b3d32a872c
mandinha2108 <amafur8@gmail.com>
2024-05-27 12:14:31 -0300

    finalizado a programação do storage

--- FramaShoppingList/src/app/home/index.js
+++ FramaShoppingList/src/app/home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Text, View, SafeAreaView, ImageBackground, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Ionicons } from '@expo/vector-icons';
@@ -9,29 +9,84 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export default function Home() {
  const [textInput, setTextInput] = useState('');
  const [items, setItems ] = useState([]);
- 
- const saveItemToDevice = async  () => {
+ useEffect(() => {
+   getItemsFromDevice();
+ }, []);
+ useEffect(() => {
+   saveItemToDevice(items);
+ }, [items]);
 
+ 
+ const saveItemToDevice = async  (items) => {
+  try {
+    const itemJson = JSON.stringify(items);
+    await AsyncStorage.setItem('FramaShoppingList', itemJson);
+  } catch (error) {
+    console.log(`Erro: ${error}`);
+  }
  }
 
  const getItemsFromDevice = async() => {
-
+  try {
+    const items = await AsyncStorage.getItem('FramaShoppingList');
+    if (items != null ) {
+      setItems(JSON.parse(items));
+    }
+ } catch (error) {
+   console.log(`Erro: ${error}`);
  }
- 
+} 
  const addItem = () => {
+   // console.log(textInput);
+   if (textInput == '') {
+    Alert.alert('Ocorreu um problema :(', 'Por favor, informe o nome do produto!!');
+   } else {
+    const newItem = {
+      id: Math.random(),
+      name : textInput,
+      bought: false
+    };
+    setItems([...items, newItem]);
+    setTextInput('');
+   }
 
  }
 
  const markItemBought = itemId => {
-
+  const newItems = items.map((item) => {
+    if (item.id == itemId){
+      return {...item, bought: true }
+    }
+    return item;
+  });
+  setItems(newItems);
  }
 
  const unmarkItemBought = itemId => {
-
+  const newItems = items.map((item) => {
+    if (item.id == itemId){
+      return {...item, bought: false }
+    }
+    return item;
+  });
+  setItems(newItems);
  }
  
+ 
  const removeItem = itemId => {
-
+    Alert.alert('Excluir Produto?', 'Confirma a exclusão deste produto?',
+    [
+      {
+        text: 'Sim', onPress: () => {
+          const newItems = items.filter(item => item.id != itemId);
+          setItems(newItems);
+        }
+      },
+      {
+        text: 'Cancelar',
+        style: 'cancel'
+      }
+    ]);
  }
 
  const removeAll = () => {
@@ -61,7 +116,7 @@ export default function Home() {
           contentContainerStyle={{ padding: 20, paddingBottom: 100, color:'#fff' }}
           data = {items}
           renderItem={({item}) => 
-            <ListItem item = {item}
+            <ItemList item = {item}
              markItem={markItemBought}
              unmarkItem={unmarkItemBought}
              removeItem={removeItem}
